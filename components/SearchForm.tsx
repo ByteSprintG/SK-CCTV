92d594dd808fa8ea967ae471f1b61adeb576af17
Sahan-Maleesha <160100516+Sahan-Maleesha@users.noreply.github.com>
2025-11-26 23:25:04 +0530

    add filters (#13)

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -5,11 +5,11 @@ import { Search } from "lucide-react";
 
 const SearchForm = ({query}: {query?: string}) =>{
     return(
-        <Form action="/products" scroll={false} className = "search-form flex flex-col md:flex-row">
+        <Form action="/products" scroll={false} className = "search-form flex flex-col md:flex-row  border-blue-400 rounded-3xl border-2 min-w-2xs  p-2 justify-center">
             <input 
                 name="query"
                 defaultValue={query}
-                className="search-input"
+                className="search-input focus:outline-none"
                 placeholder="Search Products"
              />
              <div className = "flex gap-2">
