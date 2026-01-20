92b304f761c0620a39d32042d8e585fe6648c453
kanishkadharmarathne <kanishkadewinda1102@gmail.com>
2026-01-20 21:31:31 +0530

    WIP

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,37 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'], // Allow Google user profile images
   },
+  webpack: (config, { isServer }) => {
+    // Suppress warnings for optional TypeORM dependencies
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /node_modules\/typeorm/ },
+    ];
+
+    // Ignore optional dependencies that TypeORM tries to load
+    if (isServer) {
+      config.externals = [
+        ...(typeof config.externals === 'function'
+          ? [config.externals]
+          : Array.isArray(config.externals)
+          ? config.externals
+          : []),
+        {
+          'react-native-sqlite-storage': 'react-native-sqlite-storage',
+          '@sap/hana-client': '@sap/hana-client',
+          'mysql': 'mysql',
+          'mysql2': 'mysql2',
+          'oracledb': 'oracledb',
+          'pg': 'pg',
+          'pg-native': 'pg-native',
+          'sqlite': 'sqlite',
+          'sqlite3': 'sqlite3',
+        },
+      ];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
